6fcc112eaac4e50bef12dcadf123885b064dd48d
Daniil <daniilbagautdinov@yandex.ru>
2022-03-13 17:52:32 +0300

    added camera moving on the map

--- TinkAutoParking/MapViewController.swift
+++ TinkAutoParking/MapViewController.swift
@@ -1,8 +1,47 @@
 //
-//  MapViewController.swift
-//  TinkAutoParking
 //
 //  Created by Даниил Багаутдинов on 09.03.2022.
 //
 
-import Foundation
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "MapScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
