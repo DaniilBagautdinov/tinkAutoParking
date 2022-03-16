e648162dfa6e4a6e1d709e50c3871e26f356cffb
Daniil <daniilbagautdinov@yandex.ru>
2022-03-16 19:43:53 +0300

    added zoom functionality

--- TinkAutoParking/MapScene.swift
+++ TinkAutoParking/MapScene.swift
@@ -13,11 +13,11 @@ class MapScene: SKScene {
     private var tileMapNode : SKTileMapNode?
     
     var previousCameraPoint = CGPoint.zero
+    var previousCameraScale = CGFloat()
+    
     
     override func didMove(to view: SKView) {
         self.tileMapNode = self.childNode(withName: "//tileMapNode") as? SKTileMapNode
-        self.camera = self.childNode(withName: "//camera") as? SKCameraNode
-        self.camera?.position = CGPoint(x: 1000.0, y: 2000.3)
         let panGesture = UIPanGestureRecognizer()
         panGesture.addTarget(self, action: #selector(panGestureAction(_:)))
         view.addGestureRecognizer(panGesture)
@@ -25,30 +25,44 @@ class MapScene: SKScene {
     }
     
     @objc func panGestureAction(_ sender: UIPanGestureRecognizer) {
-       // The camera has a weak reference, so test it
-       guard let camera = self.camera else {
-         return
-       }
-       // If the movement just began, save the first camera position
-       if sender.state == .began {
-         previousCameraPoint = camera.position
-       }
-       // Perform the translation
-       let translation = sender.translation(in: self.view)
-       let newPosition = CGPoint(
-         x: previousCameraPoint.x + translation.x * -1,
-         y: previousCameraPoint.y + translation.y
-       )
-       camera.position = newPosition
-     }
+        // The camera has a weak reference, so test it
+        guard let camera = self.camera else {
+            return
+        }
+        // If the movement just began, save the first camera position
+        if sender.state == .began {
+            previousCameraPoint = camera.position
+        }
+        // Perform the translation
+        let translation = sender.translation(in: self.view)
+        let newPosition = CGPoint(
+            x: previousCameraPoint.x + translation.x * -1,
+            y: previousCameraPoint.y + translation.y
+        )
+        camera.position = newPosition
+    }
     
-   
+    override func sceneDidLoad() {
+        let pinchGesture = UIPinchGestureRecognizer()
+        pinchGesture.addTarget(self, action: #selector(pinchGestureAction(_:)))
+        view?.addGestureRecognizer(pinchGesture)
+    }
+    
+    @objc func pinchGestureAction(_ sender: UIPinchGestureRecognizer) {
+        guard let camera = self.camera else {
+            return
+        }
+        if sender.state == .began {
+            previousCameraScale = camera.xScale
+        }
+        camera.setScale(previousCameraScale * 1 / sender.scale)
+    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
     }
     
-  
+    
     override func update(_ currentTime: TimeInterval) {
     }
 }
