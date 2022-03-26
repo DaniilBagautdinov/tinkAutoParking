d697011a7f3756b6ade37bcdfd2a4225af474ef9
Semyon Sokolov <simionsimion@yandex.ru>
2022-03-26 13:21:05 +0300

    Revert "Merge pull request #3 from DaniilBagautdinov/TINK-11-модуль-авторизации"
    This reverts commit 08e061672f1d08a5573700c3cfdd37a80a738ce1, reversing
    changes made to 65712ad302b7055c06413a0bb03ec3d9e0bd5d80.

--- TinkAutoParking/Authorization/AuthorizationAssembly.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AuthorizationAssembly.swift
-//  TinkAutoParking
-//
-//  Created by Семён Соколов on 26.03.2022.
-//
-
-import Foundation
--- TinkAutoParking/Authorization/Interactor/UserInteractor.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UserInteractor.swift
-//  TinkAutoParking
-//
-//  Created by Семён Соколов on 26.03.2022.
-//
-
-import Foundation
